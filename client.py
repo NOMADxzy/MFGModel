af9db86d61e17ef4c80d5d9c9a93604fd4478b3b
NOMADxzy <3302309675@qq.com>
2024-04-17 11:35:58 +0800

    fix: 文件方式共享状态

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import indigo_pb2
 import indigo_pb2_grpc
 
 def run():
-    channel = grpc.insecure_channel('localhost:50054')
+    channel = grpc.insecure_channel('localhost:50053')
     stub = indigo_pb2_grpc.acerServiceStub(channel)
     response = stub.GetExplorationAction(indigo_pb2.State(delay=20, delivery_rate=20, send_rate=20, cwnd=20, port=8080))
     # response = stub.GetExplorationAction(indigo_pb2.State(delay=10, delivery_rate=10, send_rate=10, cwnd=10, port=8081))
--- rlServer.py
+++ rlServer.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 
+import os.path
 import subprocess
 from concurrent import futures
 import gc, grpc
@@ -23,6 +24,7 @@ logging.basicConfig(level=logging.INFO)
 # 创建一个锁对象
 lock = FileLock("state.lock")
 
+
 # lock = threading.Lock()
 
 # trainer.load_models(2800,s=1,v=15)
@@ -34,7 +36,9 @@ def format_actions(action_list):
     the values are lists ['op', val], such as ['+', '2.0'].
     """
     return {idx: [action[0], float(action[1:])]
-                  for idx, action in enumerate(action_list)}
+            for idx, action in enumerate(action_list)}
+
+
 class RLmethods(indigo_pb2_grpc.acerServiceServicer):
 
     def __init__(self):
@@ -49,12 +53,12 @@ class RLmethods(indigo_pb2_grpc.acerServiceServicer):
 
         # self.client_num = 0
         # self.client_states = {}
-        self.pre_state = [0,0,0,0]
+        self.pre_state = [0, 0, 0, 0]
 
         self.action_mapping = format_actions(["/2.0", "-10.0", "+0.0", "+10.0", "*2.0"])
         self.action_cnt = len(self.action_mapping)
 
-        self.phi = 0.1 # 自己状态所占的比例
+        self.phi = 0.1  # 自己状态所占的比例
 
     def load_model(self):
         model_path = path.join(project_root.DIR, 'a3c', 'logs', 'model')
@@ -67,7 +71,7 @@ class RLmethods(indigo_pb2_grpc.acerServiceServicer):
         self.sample_action = self.learner.sample_action
 
     def overly(self, cur_state, avg_state):
-        new_state = [0,0,0,0]
+        new_state = [0, 0, 0, 0]
         new_state[0] = cur_state[0] * self.phi + avg_state[0] * (1.0 - self.phi)
         new_state[1] = cur_state[1] * self.phi + avg_state[1] * (1.0 - self.phi)
         new_state[2] = cur_state[2] * self.phi + avg_state[2] * (1.0 - self.phi)
@@ -86,7 +90,6 @@ class RLmethods(indigo_pb2_grpc.acerServiceServicer):
             avg_state = self.SyncFile(self.pre_state, cur_state, False)
             port = state.port
 
-
             input_state = self.overly(cur_state, avg_state)
 
             action = self.sample_action(input_state)
@@ -99,7 +102,6 @@ class RLmethods(indigo_pb2_grpc.acerServiceServicer):
             self.pre_state = cur_state
             print "target_cwnd: " + str(target_cwnd)
             return indigo_pb2.Action(action=target_cwnd)
-
 
     # def update_states(self, state):
     #     port = state.port
@@ -132,6 +134,7 @@ class RLmethods(indigo_pb2_grpc.acerServiceServicer):
                                     cwnd=input_state[3], port=state.port)
 
     def SyncFile(self, pre_state, state, read=False):
+        self.check_file()
         with open('states.txt', 'r') as file:
             # 从文件中读取内容，并按空格分割字符串，然后将其转换为浮点数列表
             avg_states = [float(num) for num in file.readline().split()]
@@ -140,10 +143,10 @@ class RLmethods(indigo_pb2_grpc.acerServiceServicer):
             return avg_states
         else:
             new_avg_state = [0 for _ in range(4)]
-            new_avg_state[0] = avg_states[0]*sender_num + state[0] - pre_state[0]
-            new_avg_state[1] = avg_states[1]*sender_num + state[1] - pre_state[1]
-            new_avg_state[2] = avg_states[2]*sender_num + state[2] - pre_state[2]
-            new_avg_state[3] = avg_states[3]*sender_num + state[3] - pre_state[3]
+            new_avg_state[0] = avg_states[0] * sender_num + state[0] - pre_state[0]
+            new_avg_state[1] = avg_states[1] * sender_num + state[1] - pre_state[1]
+            new_avg_state[2] = avg_states[2] * sender_num + state[2] - pre_state[2]
+            new_avg_state[3] = avg_states[3] * sender_num + state[3] - pre_state[3]
             if pre_state[0] == 0:
                 sender_num += 1
             for i in range(len(new_avg_state)):
@@ -154,6 +157,13 @@ class RLmethods(indigo_pb2_grpc.acerServiceServicer):
                     file.write("{} ".format(num))
                 file.write("\n{}".format(sender_num))
             return new_avg_state
+
+    def check_file(self, file_path="states.txt"):
+        if os.path.exists(file_path):
+            return
+        data = """0 0 0 0\n0"""
+        with open(file_path, 'w') as file:
+            file.write(data)
 
 
 def main():
